83d791e76831337774056e61c663d401f9d59855
benmsvgrkf-web <benmsvgrkf@gmail.com>
2025-12-23 15:34:56 +0700

    Update penyedia.js

--- src/endpoints/penyedia.js
+++ src/endpoints/penyedia.js
@@ -3,7 +3,7 @@ import { fetchAll } from "../services/fetch_all.js";
 
 export async function getPenyedia(tahun, kodeKLPD) {
   const url =
-    `${BASE_URL}/api/v1/rup/paket-penyedia-terumumkan` +
+    `${BASE_URL}/api/v1/rup/rencana/penyedia` +
     `?tahun=${tahun}&kode_klpd=${kodeKLPD}&limit=100`;
 
   return await fetchAll(url);
