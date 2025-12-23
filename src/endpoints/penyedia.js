5bb5bd04f888a828971ac83b0c9ae2e507c8cc89
benmsvgrkf-web <benmsvgrkf@gmail.com>
2025-12-23 13:32:17 +0700

    add penyedia endpoint

---
+++ src/endpoints/penyedia.js
@@ -0,0 +1,10 @@
+import { BASE_URL } from "../config/inaproc.js";
+import { fetchAll } from "../services/fetchAll.js";
+
+export async function getPenyedia(tahun, kodeKLPD) {
+  const url =
+    `${BASE_URL}/api/v1/rup/paket-penyedia-terumumkan` +
+    `?tahun=${tahun}&kode_klpd=${kodeKLPD}&limit=100`;
+
+  return await fetchAll(url);
+}
