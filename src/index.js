bdb1adfe8f0c72c598412cd8d23c063eb13a626a
benmsvgrkf-web <benmsvgrkf@gmail.com>
2025-12-23 15:48:02 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import { writeSheet } from "./sheets/writeSheet.js";
 
 async function main() {
   const tahun = 2026;
-  const kodeKLPD = "D101"; // <-- KODE KLPD KAMU
+  const bulan = 1; // <-- WAJIB ANGKA 1–12
 
   console.log("Sync Penyedia...");
-  const penyedia = await getPenyedia(tahun, kodeKLPD);
-  console.log("Jumlah data:", penyedia.length);
+  const penyedia = await getPenyedia(tahun, bulan);
 
+  console.log("Jumlah data:", penyedia.length);
   await writeSheet("Penyedia", penyedia);
 }
 
