c216bb343418bb69e1d1958db261c35d2d98ddc2
benmsvgrkf-web <benmsvgrkf@gmail.com>
2025-12-23 14:19:56 +0700

    Update fetchAll.js

--- src/services/fetchAll.js
+++ src/services/fetchAll.js
@@ -1,6 +1,6 @@
 import { HEADERS } from "../config/inaproc.js";
 
-export async function fetchAll(url) {
+export default async function fetchAll(url) {
   let allData = [];
   let cursor = null;
   let hasMore = true;
@@ -12,7 +12,9 @@ export async function fetchAll(url) {
     const res = await fetch(u, { headers: HEADERS });
     const json = await res.json();
 
-    allData.push(...json.data);
+    if (Array.isArray(json.data)) {
+      allData.push(...json.data);
+    }
 
     cursor = json.meta?.cursor;
     hasMore = json.meta?.has_more;
