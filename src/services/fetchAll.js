ad261a1682de1ae5c7d21c9e4feba2392e21c876
benmsvgrkf-web <benmsvgrkf@gmail.com>
2025-12-23 13:29:14 +0700

    add fetchAll helper

---
+++ src/services/fetchAll.js
@@ -0,0 +1,24 @@
+import { HEADERS } from "../config/inaproc.js";
+
+export async function fetchAll(url) {
+  let allData = [];
+  let cursor = null;
+  let hasMore = true;
+
+  while (hasMore) {
+    const u = new URL(url);
+    if (cursor) {
+      u.searchParams.set("cursor", cursor);
+    }
+
+    const res = await fetch(u, { headers: HEADERS });
+    const json = await res.json();
+
+    allData.push(...json.data);
+
+    cursor = json.meta?.cursor;
+    hasMore = json.meta?.has_more;
+  }
+
+  return allData;
+}
