5328e0ab18e6837318aaf07fa332787d737370b7
benmsvgrkf-web <benmsvgrkf@gmail.com>
2025-12-23 15:42:10 +0700

    Update penyedia.js

--- src/endpoints/penyedia.js
+++ src/endpoints/penyedia.js
@@ -1,7 +1,9 @@
 import { BASE_URL } from "../config/inaproc.js";
 import { fetchAll } from "../services/fetch_all.js";
 
-export async function getPenyedia(tahun, bulan, lpseId) {
+export async function getPenyedia(tahun, bulan) {
+  const lpseId = 163;
+
   const url =
     `${BASE_URL}/v1/rup/rencana/penyedia` +
     `?lpse_id=${lpseId}&tahun=${tahun}&bulan=${bulan}&limit=100`;
