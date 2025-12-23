08faf156586a3f97b9b8fc1344ba1c8e4ca80d3b
benmsvgrkf-web <benmsvgrkf@gmail.com>
2025-12-23 14:02:19 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,15 @@
 import { getPenyedia } from "./endpoints/penyedia.js";
-import { getSwakelola } from "./endpoints/swakelola.js";
 import { writeSheet } from "./sheets/writeSheet.js";
 
 async function main() {
   const tahun = 2025;
-  const kodeKLPD = "30";
+  const kodeKLPD = "D101"; // <-- KODE KLPD KAMU
 
-  console.log("Sync Penyedia");
+  console.log("Sync Penyedia...");
   const penyedia = await getPenyedia(tahun, kodeKLPD);
+  console.log("Jumlah data:", penyedia.length);
+
   await writeSheet("Penyedia", penyedia);
-
-  console.log("Sync Swakelola");
-  const swakelola = await getSwakelola(tahun, kodeKLPD);
-  await writeSheet("Swakelola", swakelola);
-
-  // 👉 lanjutkan:
-  // Tender
-  // Non Tender
-  // E-Purchasing
-  // Toko Daring
 }
 
 main().catch(console.error);
