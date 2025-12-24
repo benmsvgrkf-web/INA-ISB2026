78e3c5a575b59b0beb30a347fdfeb56ae80a15f5
benmsvgrkf-web <benmsvgrkf@gmail.com>
2025-12-24 13:11:46 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
-import { getPenyedia } from "./endpoints/penyedia.js";
 import { writeSheet } from "./sheets/writeSheet.js";
 
 async function main() {
-  const tahun = 2026;
+  const dummy = [
+    { kolom1: "TES", kolom2: 123 },
+    { kolom1: "MASUK", kolom2: 456 }
+  ];
 
-  console.log("Sync Penyedia...");
-  const data = await getPenyedia(tahun);
-
-  console.log("TOTAL DATA:", data.length);
-  await writeSheet("Penyedia", data);
+  await writeSheet("Penyedia", dummy);
 }
 
 main().catch(console.error);
